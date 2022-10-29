654a98dec06d18f6f4f596cbcd0e0820ee67989d
mryhryki <mryhryki@gmail.com>
2022-10-29 18:45:34 +0900

    Update pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          I&apos;m <a href="https://mryhryki.com">MORIYA Hiroyuki</a>.
         </h1>
 
         <p className={styles.description}>
